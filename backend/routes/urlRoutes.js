00ad14f12d0b4aaa1fa12166d67d0a3db53263b4
Nawang Dorjee <154888348+Reizhea@users.noreply.github.com>
2024-12-24 21:21:35 +0530

    Added custom codes

--- backend/routes/urlRoutes.js
+++ backend/routes/urlRoutes.js
@@ -3,24 +3,50 @@ const router = express.Router();
 import { nanoid } from 'nanoid';
 import URL from '../models/URL.js';
 
+
 router.post('/shorten', async (req, res) => {
-    const { originalUrl } = req.body;
+    const { originalUrl, customCode } = req.body;
   
     if (!originalUrl) {
       return res.status(400).json({ error: 'Original URL is required' });
     }
   
+    const baseUrl = req.protocol + '://' + req.get('host');
+  
     try {
-      const shortCode = nanoid(8);
+      let shortCode;
+  
+      if (customCode) {
+        const existingCode = await URL.findOne({ shortCode: customCode });
+        if (existingCode) {
+          return res.status(400).json({ error: 'Custom code is already in use.' });
+        }
+        shortCode = customCode;
+  
+        const newUrl = new URL({ originalUrl, shortCode });
+        await newUrl.save();
+        const shortUrl = `${baseUrl}/${shortCode}`;
+        return res.json({ shortUrl });
+      }
+  
+      let existingUrl = await URL.findOne({ originalUrl }).select('shortCode');
+      if (existingUrl) {
+        const shortUrl = `${baseUrl}/${existingUrl.shortCode}`;
+        return res.json({ shortUrl });
+      }
+  
+      shortCode = nanoid(8);
+  
       const newUrl = new URL({ originalUrl, shortCode });
       await newUrl.save();
-      const baseUrl = req.protocol + '://' + req.get('host');
+  
       const shortUrl = `${baseUrl}/${shortCode}`;
       res.json({ shortUrl });
     } catch (err) {
       res.status(500).json({ error: 'Server error' });
     }
   });
+  
 
 router.get('/:shortCode', async (req, res) => {
   const { shortCode } = req.params;
@@ -28,7 +54,6 @@ router.get('/:shortCode', async (req, res) => {
   try {
     const url = await URL.findOne({ shortCode });
     if (url) {
-      url.clicks += 1;
       await url.save();
       return res.redirect(url.originalUrl);
     } else {

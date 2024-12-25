4f1188ed2d6c34127061268f29e2be7d13b9eace
Nawang Dorjee <154888348+Reizhea@users.noreply.github.com>
2024-12-25 19:50:48 +0530

    Worked out the 404 redirection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ export default function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/404" element={<Error404 />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
     </Router>
